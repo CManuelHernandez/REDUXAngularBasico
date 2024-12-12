a1a62b077622695c464a20934cd79dfb52cba237
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-12-12 11:29:02 +0100

    tore DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 //NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { contadorReducer } from './contador/contador.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,7 @@ import { NietoComponent } from './contador/nieto/nieto.component';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
   bootstrap: [AppComponent],
